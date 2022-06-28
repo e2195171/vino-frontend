4196c40e7a2c27fc4a5b7d552880f80cb6733d1f
DmitriDV <ddudchenko@gmail.com>
2022-06-27 23:30:16 -0400

    La fonction ModifierUsager.

--- src/app/dialog-modif-usager/dialog-modif-usager.component.ts
+++ src/app/dialog-modif-usager/dialog-modif-usager.component.ts
@@ -16,19 +16,19 @@ export class DialogModifUsagerComponent implements OnInit {
     modifierProfilForm!: FormGroup;
     getVilleId: any;
     villes: any;
-    ceUsager: any;
 
     constructor(
                     private formBuilder: FormBuilder,
                     public dialogRef: MatDialogRef<DialogModifUsagerComponent>,
-                    @Inject(MAT_DIALOG_DATA) public editData: IUsager,
+                   // @Inject(MAT_DIALOG_DATA) public editData: IUsager,
                     private bieroServ: ApibieroService
                 ) { }
     
-  /** Modèles d'expression régulière */
+    /** Modèles d'expression régulière */
     //courrielRegex = /(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/;
     //passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
     //confirmRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    
     
     /** Test version */
     nomRegex = /[^\s]/;
@@ -39,7 +39,18 @@ export class DialogModifUsagerComponent implements OnInit {
     passwordRegex = /[^\s]/;   
     confirmRegex = /[^\s]/;
 
+    
+
     ngOnInit(): void {
+        this.modifierProfilForm = this.formBuilder.group({
+            nom: ['', [Validators.pattern(this.nomRegex)]],
+            prenom: ['', [Validators.pattern(this.prenomRegex)]],
+            courriel: ['', [Validators.pattern(this.courrielRegex)]],
+            phone: ['', [Validators.pattern(this.phoneRegex)]],
+            adresse: ['', [Validators.pattern(this.adresseRegex)]],
+            id_ville: ['', [Validators.required]],
+        });
+
         const id = sessionStorage.id_usager;
         /** Forme et validation des données saisies */
         this.bieroServ.getListeVilles().subscribe((data: any) => { this.villes = data.data; })
@@ -48,39 +59,38 @@ export class DialogModifUsagerComponent implements OnInit {
         this.bieroServ.getProfil(id_usager)
         .subscribe({
             next: (res) => {
-                this.usager = res;
+                this.usager = res.data[0];
+                
                 console.log(this.usager);
-                this.modifierProfilForm.patchValue({
-                    courriel: this.usager.courriel,
-                    
-                    
-                  });console.log(this.modifierProfilForm.value);
+                /** Forme */
+                // this.modifierProfilForm = new FormGroup ({
+                //     nom_usager: new FormControl(this.usager['nom_usager']),
+                //     prenom: new FormControl(this.usager['prenom']),
+                //     courriel: new FormControl(this.usager['courriel']),
+                //     phone: new FormControl(this.usager['phone']),
+                //     adresse_usager: new FormControl(this.usager['adresse_usager']),
+                //     id_ville: new FormControl(this.usager['id_ville'])                    
+                // })
+
+                this.modifierProfilForm = this.formBuilder.group({
+                    nom: [this.usager.nom_usager],
+                    prenom: [this.usager.prenom],
+                    courriel: [this.usager.courriel],
+                    phone: [this.usager.phone],
+                    adresse: [this.usager.adresse_usager],
+                    id_ville: [this.usager.id_ville]
+                })
+                console.log(this.modifierProfilForm.value);
+                
+                // this.modifierProfilForm.patchValue({
+                //     courriel: this.usager.courriel,
+                //   });console.log(this.modifierProfilForm.value);
             },
             error:(err)=>{
                 alert("erreur")
             }
         })
 
-
-        this.modifierProfilForm = this.formBuilder.group({
-            nom_usager: ['', [Validators.pattern(this.nomRegex)]],
-            prenom: ['', [Validators.pattern(this.prenomRegex)]],
-            courriel: ['', [Validators.pattern(this.courrielRegex)]],
-            phone: ['', [Validators.pattern(this.phoneRegex)]],
-            adresse_usager: ['', [Validators.pattern(this.adresseRegex)]],
-            id_ville: ['', [Validators.required]],
-        });
-
-        /** Affectation des données du formulaire aux valeurs à envoyer à la base de données */
-        //if(this.editData){
-            
-            this.modifierProfilForm.controls['nom_usager'].patchValue(this.usager.nom_usager);
-            this.modifierProfilForm.controls['prenom'].patchValue(this.editData.prenom);
-            this.modifierProfilForm.controls['courriel'].patchValue(this.editData.courriel);
-            this.modifierProfilForm.controls['phone'].patchValue(this.editData.phone);
-            this.modifierProfilForm.controls['adresse_usager'].patchValue(this.editData.adresse_usager);
-            this.modifierProfilForm.controls['id_ville'].patchValue(this.editData.id_ville);
-        //}
     }
 
     /** Envoi de nouvelles données du formulaire vers la base de données */
